25f4d4fb21ff1790b8e3ffcc7311cb50c4ae09c6
Minh Tri <epsminhtri@gmail.com>
2024-02-14 23:28:34 +0700

    update val 2024

--- 8-3-2023/config.js
+++ 8-3-2023/config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Thu Thẻo';
-const giftUrl = 'http://thao-mylovestory.site/';
+const giftUrl = 'http://thao-mylovestories.click/';
 const eventName = 'Gửi Cục Lẫy';
 const titleCard = 'Cục Lẫy yêu dấu của Anh';
 const contentCard = 'Chúc Cục Lẫy của anh 8-3 tràn ngập niềm vui và nụ cười, luôn mạnh khoẻ và ít lẫy :v. Mong điều đẹp nhất sẽ đến với Em trong hôm nay và cả những ngày sau';
